286bd99b7e3717f689f06ba4fe99200a542df3c9
maxicifu <111088621+maxicifu@users.noreply.github.com>
2023-03-07 15:29:22 -0300

    Add files via upload

---
+++ Portafolio.js
@@ -0,0 +1,12 @@
+document.getElementById('boton_maderanegra').addEventListener('click', function(){
+    document.body.style.backgroundImage = "url('https://www.todofondos.net/wp-content/uploads/fondo-de-pantalla-de-iphone-6-wood-fondo-de-pantalla-de-iphone-en-2019-papel-pintado-de-madera.-imagen-de-madera-577x1024.jpg')";
+    console.log("Estamos en byid boton_maderanegra");
+})
+document.getElementById('boton_maderamarron').addEventListener('click', function(){
+    document.body.style.backgroundImage = "url('https://www.todofondos.net/wp-content/uploads/fondos-de-madera-los-mejores-fondos-de-madera-gratis-wallpaperaccess.-fondo-para-computadora-de-madera-768x613.jpg')";
+    console.log("Estamos en byid boton_maderamarron");
+})
+document.getElementById('boton_maderablanca').addEventListener('click', function(){
+    document.body.style.backgroundImage = "url('https://www.todofondos.net/wp-content/uploads/papel-pintado-de-madera-pescado-ideco-arena.-wallpaper-de-madera-768x768.jpg')";
+    console.log("Estamos en byid boton_maderablanca");
+})
